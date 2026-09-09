b4f9f6a03ec70ec6105d5f4479bf3823a98e1584
shariom1008 <shariom1008@gmail.com>
2026-09-09 19:25:41 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,3 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
-dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-}
