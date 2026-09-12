f2f9f557a24420b7166472b309c35251d366fa8a
shariom1008 <shariom1008@gmail.com>
2026-09-13 02:32:45 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-ktx:1.8.2")
 implementation("com.google.firebase:firebase-auth:23.1.0")
+implementation("androidx.credentials:credentials:1.3.0")
+implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
+implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
 }
