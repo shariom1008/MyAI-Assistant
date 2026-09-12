096a2b45ec749dd456b0bbd50b99b5b0be553d9d
shariom1008 <shariom1008@gmail.com>
2026-09-12 11:08:08 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/myaiassistant/MainActivity.kt
+++ app/src/main/java/com/example/myaiassistant/MainActivity.kt
@@ -153,6 +153,32 @@ class MainActivity : Activity() {
     }
 
     // =========================================================
+    // MICROPHONE PERMISSION
+    // =========================================================
+
+private fun requestMicrophonePermission() {
+
+    if (
+        ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.RECORD_AUDIO
+        ) != PackageManager.PERMISSION_GRANTED
+    ) {
+
+        requestPermissions(
+            arrayOf(
+                Manifest.permission.RECORD_AUDIO
+            ),
+            500
+        )
+
+    } else {
+
+        startAurixService()
+    }
+}
+
+    // =========================================================
     // MAIN UI
     // =========================================================
 
