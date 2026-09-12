5897af91c22c47fd0a5dbc8a6ec54f0fe69c56f9
shariom1008 <shariom1008@gmail.com>
2026-09-13 01:55:31 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,5 @@ buildConfigField(
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-ktx:1.8.2")
-implementation(platform("com.google.firebase:firebase-bom:34.18.0"))
-implementation("com.google.firebase:firebase-auth")
+implementation("com.google.firebase:firebase-auth:23.1.0")
 }
